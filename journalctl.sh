b059250f523cef9840606913a2d1c9ea01c62f1a
ps-chris-jackson <97684725+cjacksonACG@users.noreply.github.com>
2025-10-06 19:18:23 -0400

    Delete journalctl.sh

--- journalctl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-LOG_FILE="journalctl.log"
-sudo journalctl > "$LOG_FILE"
-
-# Replace with your actual bucket name
-S3_BUCKET="s3://s3-bucket-name"
-
-aws s3 cp "$LOG_FILE" "$S3_BUCKET"
